a3c9cb07ae524e369de4d09337d6dfc08a19eab3
Sharmarke Duale <sduale01@gmail.com>
2019-01-28 08:03:32 -0600

    send survey to the databse via axios.post

---
+++ src/components/Review/Lastpage.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Lastpage extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Thank You!</h1>
+                <button>Leave New Feedback</button>
+            </div>
+        );
+    }
+}
+
+export default Lastpage;
