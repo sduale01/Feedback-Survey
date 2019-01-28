25a54ef88ca74ee1b0d2d55d856d81803977d799
Sharmarke Duale <sduale01@gmail.com>
2019-01-28 08:32:39 -0600

    reset form

--- src/components/Review/Lastpage.js
+++ src/components/Review/Lastpage.js
@@ -1,14 +1,25 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 
 class Lastpage extends Component {
+
+    // This will reset the form so new feedback can be filled out.
+    handleReset = () => {
+        console.log('leave new feedback clicked');
+        
+        const action = {type: 'RESET_FORM'}
+        this.props.dispatch(action)
+        this.props.history.push('/')
+    }
+
     render() {
         return (
             <div>
                 <h1>Thank You!</h1>
-                <button>Leave New Feedback</button>
+                <button onClick={this.handleReset}>Leave New Feedback</button>
             </div>
         );
     }
 }
 
-export default Lastpage;
+export default connect()(Lastpage);
